5bc677d194a1151efab60bef3178d8af4bd46822
Yeonju Kim <kyeonju23@gmail.com>
2023-03-06 16:31:34 +0900

    server init

---
+++ server/src/main/java/com/project/chamong/ChamongApplication.java
@@ -0,0 +1,13 @@
+package com.project.chamong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamongApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/project/chamong/ChamongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.chamong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
