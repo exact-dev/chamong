fed644059b899d630149f9b10a3352c833a30f10
exact-dev <115613439+exact-dev@users.noreply.github.com>
2023-03-21 16:36:52 +0900

    Delete server/src/main/java/com/project/chamong/community directory

--- server/src/main/java/com/project/chamong/community/repository/ArticleLikeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.chamong.community.repository;
-
-import com.project.chamong.community.entity.ArticleLike;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
-}
